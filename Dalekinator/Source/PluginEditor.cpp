45996cbd9e4fdde44ef869ab55dce49d728f68cb
Andrew Wyld <andrew.wyld@gmail.com>
2026-05-26 10:46:20 +0100

    OPTICAL MODIFICATION COMPLETE
    PREVIOUS BOUNDS ELIMINATED

--- Dalekinator/Source/PluginEditor.cpp
+++ Dalekinator/Source/PluginEditor.cpp
@@ -17,8 +17,8 @@ DalekinatorAudioProcessorEditor::DalekinatorAudioProcessorEditor (DalekinatorAud
     // editor's size to whatever you need it to be.
     setSize (400, 300);
     addAndMakeVisible(frequencySlider);
-    frequencySlider.setRange (0.02, 20000.0);
-    frequencySlider.setSkewFactorFromMidPoint(30); // multiplicative midpoint
+    frequencySlider.setRange (0.2, 20000.0, 0.01);
+    frequencySlider.setSkewFactorFromMidPoint(0.2 * sqrt(100000.0)); // multiplicative midpoint
     frequencySlider.setTextValueSuffix (" Hz");
     frequencySlider.addListener (this);
     frequencySlider.setValue(audioProcessor.getOscillatorFrequency());
@@ -39,13 +39,12 @@ void DalekinatorAudioProcessorEditor::sliderValueChanged(juce::Slider* s)
 //==============================================================================
 void DalekinatorAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    frequencySlider.setBounds(getLocalBounds());
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    // g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    // g.setColour (juce::Colours::white);
-    // g.setFont (juce::FontOptions (15.0f));
-    // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (15.0f));
+
+    frequencySlider.setBounds(getLocalBounds());
 }
 
 void DalekinatorAudioProcessorEditor::resized()
